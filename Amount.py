267039b5c606ba0b1932b1b9bfa7bbb4fb289679
thomasgalley <thomas.galley.14@ucl.ac.uk>
2015-02-26 18:16:38 +0000

    Added some conversion capabilities

--- Amount.py
+++ Amount.py
@@ -1,15 +1,20 @@
 class Amount:
-	def __init__(self,quantity='',unit='',coefficient=0):
-		self.quantity=quantity
-		self.unit=unit
-		self.coefficient=coefficient
-	def metertocentimetre(self):
-            if self.quantity=='length': 
-                if self.unit=='metre':
-                    self.coefficient=100*self.coefficient
-	def printcoeff(self):
-         print(self.coefficient)
-	def add(self, others):
+    def __init__(self,quantity='',unit='',coefficient=0):
+        self.quantity=quantity
+        self.unit=unit
+        self.coefficient=coefficient
+    def add(self, others):
             if self.quantity==others.quantity:
                 if self.unit==others.unit:
                     return self.coefficient+others.coefficient
+            else:
+                raise ValueError('Incompatibleunits')
+    def convert(self,conversionunit):
+        if self.unit=='meter' and conversionunit=='kilometer':
+            return 0.001*self.coefficient
+        elif self.unit=='kilometer' and conversionunit=='meter':
+            return 1000*self.coefficient
+        else:
+            return self.coefficient
+    def test(self,t=0):
+        return self.coefficient*t
