118ef0f5e65dd15bd1fd839f69e14760be0b211f
thomasgalley <thomas.galley.14@ucl.ac.uk>
2015-02-27 16:38:17 +0000

    Added unit conversion test

--- test_units.py
+++ test_units.py
@@ -3,3 +3,6 @@ from Amount import*
 def test_equalities():
     assert_true(Amount('length','meters',5)==Amount('length','kilometers',0.005))
     assert_true(Amount('time','seconds',60)==Amount('time','minutes',1))
+
+def test_conversion():
+    assert_true(Amount('time','seconds',60).convert('minutes')==1.0)
